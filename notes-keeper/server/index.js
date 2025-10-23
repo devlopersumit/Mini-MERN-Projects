08a3a8c9a063e28177e2281d1afdbaa01e23cbc8
devlopersumit <sumitsj2002@gmail.com>
2025-10-23 19:35:29 +0530

    Connected MongoDB Successfully

--- notes-keeper/server/index.js
+++ notes-keeper/server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("✅ MongoDB Connected Succesfully");
+    
+}).catch((err) => {
+    console.error("MongoDB Connection Error: ", err);
+});
 
 app.get('/', (req, res) => {
     res.send("Hello World");

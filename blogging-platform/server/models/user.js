61570ccc2cd80a40a10a09ba2454aad632766d70
devlopersumit <sumitsj2002@gmail.com>
2025-10-31 16:33:21 +0530

    Crrated user schema

---
+++ blogging-platform/server/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require("email-validator");
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:[true, 'userName is required'],
+        minLength:3,
+        maxLength:10,
+        trim:true
+    },
+
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: (value) => validator.validate(value), 
+      message: "Please enter a valid email address",
+    },
+  },
+
+  password:{
+    type:String,
+    required:[true,'Password is required'],
+    minLength:[6,'password must be atleast 6 characters']
+  },
+
+  avatarUrl:{
+    type:String,
+  },
+
+  bio:{
+    type:String,
+    maxLength:100,
+  },
+}, {
+    timestamps:true
+});
+
+module.exports = mongoose.model("User", userSchema);
